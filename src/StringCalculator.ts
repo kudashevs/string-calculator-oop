61150dc5601aa353cfa3416aada7ee4919472ea8
Sergey Kudashev <kudashevs@gmail.com>
2025-03-26 14:42:50 +0300

    Refactoring extract a parseInput method

--- src/StringCalculator.ts
+++ src/StringCalculator.ts
@@ -1,24 +1,32 @@
 export class StringCalculator {
+  private delimiters: string;
+  private numbers: number[];
+
   add(numbers: string): number {
     if (numbers.length === 0) {
       return 0;
     }
 
-    let delimiter: string = '[,\n]';
-    if (numbers.startsWith('//')) {
-      const parts = numbers.split('\n');
-      delimiter = parts[0].substring(2).replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-      numbers = parts[1];
-    }
+    this.parseInput(numbers);
 
-    const individualNumbers = this.prepareNumbers(numbers, delimiter);
-
-    return individualNumbers.reduce((result, n) => result + n);
+    return this.numbers.reduce((result, n) => result + n);
   }
 
-  private prepareNumbers(input: string, delimiter: string): number[] {
+  private parseInput(input: string): void {
+    if (input.startsWith('//')) {
+      const parts = input.split('\n');
+
+      this.delimiters = parts[0].substring(2).replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+      this.numbers = this.prepareNumbers(parts[1]);
+    } else {
+      this.delimiters = '[,\n]';
+      this.numbers = this.prepareNumbers(input);
+    }
+  }
+
+  private prepareNumbers(input: string): number[] {
     return input
-      .split(new RegExp(delimiter))
+      .split(new RegExp(this.delimiters))
       .map(n => parseInt(n));
   }
 }
