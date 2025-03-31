282b9a07b5097d068849322024d42490456d5d95
Sergey Kudashev <kudashevs@gmail.com>
2025-03-31 21:09:54 +0300

    Fifth step second iteration

--- src/Validators/DefaultValidator.ts
+++ src/Validators/DefaultValidator.ts
@@ -3,8 +3,10 @@ import {Validator} from './Validator';
 export class DefaultValidator implements Validator {
   validate(numbers: number[]): void {
     numbers.forEach((n) => {
-      if (n < 0) {
-        throw new Error(`negatives not allowed: ${n}`);
+      const invalidNumbers: number[] = numbers.filter((n) => n < 0);
+
+      if (invalidNumbers.length > 0) {
+        throw new Error(`negatives not allowed: ${invalidNumbers.join(',')}`);
       }
     });
   }
