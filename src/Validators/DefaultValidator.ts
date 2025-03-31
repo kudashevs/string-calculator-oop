d58a676ac461ec42659b3707dcdf52e0d548ab9e
Sergey Kudashev <kudashevs@gmail.com>
2025-03-31 21:10:18 +0300

    Refactoring extract a validation message constant

--- src/Validators/DefaultValidator.ts
+++ src/Validators/DefaultValidator.ts
@@ -1,12 +1,14 @@
 import {Validator} from './Validator';
 
 export class DefaultValidator implements Validator {
+  static readonly VALIDATION_MESSAGE = 'negatives not allowed';
+
   validate(numbers: number[]): void {
     numbers.forEach((n) => {
       const invalidNumbers: number[] = numbers.filter((n) => n < 0);
 
       if (invalidNumbers.length > 0) {
-        throw new Error(`negatives not allowed: ${invalidNumbers.join(',')}`);
+        throw new Error(DefaultValidator.VALIDATION_MESSAGE + ': ' + invalidNumbers.join(','));
       }
     });
   }
